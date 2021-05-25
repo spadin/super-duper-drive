22d9b636bfc39fe7346b24fcaa461f2b6a83a705
Sandro Padin <sandropadin@gmail.com>
2021-05-25 06:11:33 -0700

    Reformat test file

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,7 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,31 +14,30 @@ import org.springframework.boot.web.server.LocalServerPort;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
-    @LocalServerPort
-    private Integer port;
+  @LocalServerPort private Integer port;
 
-    private WebDriver driver;
+  private WebDriver driver;
 
-    @BeforeAll
-    static void beforeAll() {
-        WebDriverManager.chromedriver().setup();
+  @BeforeAll
+  static void beforeAll() {
+    WebDriverManager.chromedriver().setup();
+  }
+
+  @BeforeEach
+  public void beforeEach() {
+    this.driver = new ChromeDriver();
+  }
+
+  @AfterEach
+  public void afterEach() {
+    if (this.driver != null) {
+      driver.quit();
     }
+  }
 
-    @BeforeEach
-    public void beforeEach() {
-        this.driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void afterEach() {
-        if (this.driver != null) {
-            driver.quit();
-        }
-    }
-
-    @Test
-    public void getLoginPage() {
-        driver.get("http://localhost:" + this.port + "/login");
-        Assertions.assertEquals("Login", driver.getTitle());
-    }
+  @Test
+  public void getLoginPage() {
+    driver.get("http://localhost:" + this.port + "/login");
+    Assertions.assertEquals("Login", driver.getTitle());
+  }
 }
