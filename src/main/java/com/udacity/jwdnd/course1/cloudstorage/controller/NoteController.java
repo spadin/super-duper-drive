a61efcc3e5118ef2ba6519d1a7ba3814e9705882
Sandro Padin <sandropadin@gmail.com>
2021-05-26 21:37:48 -0700

    Refactor to avoid try/catch and result logic in same method

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.Result;
 import com.udacity.jwdnd.course1.cloudstorage.model.ResultFactory;
 import com.udacity.jwdnd.course1.cloudstorage.services.AlreadyExistsException;
 import com.udacity.jwdnd.course1.cloudstorage.services.DataTooLargeException;
@@ -26,22 +27,9 @@ public class NoteController {
   @PostMapping
   public String createOrUpdateNote(
       @ModelAttribute Note note, RedirectAttributes redirectAttributes) {
-    try {
-      this.noteService.createOrUpdateNote(note);
-      redirectAttributes.addFlashAttribute(
-          "result", ResultFactory.createSuccessResult("Note successfully created or updated."));
-    } catch (DataTooLargeException e) {
-      redirectAttributes.addFlashAttribute(
-          "result",
-          ResultFactory.createErrorResult("Note contents too large. Please make note smaller."));
-    } catch (AlreadyExistsException e) {
-      redirectAttributes.addFlashAttribute(
-          "result",
-          ResultFactory.createErrorResult(
-              "Note already exists. Please edit or delete the note instead."));
-    } finally {
-      return "redirect:/result";
-    }
+    Result result = this.getResultForCreateOrUpdateNote(note);
+    redirectAttributes.addFlashAttribute("result", result);
+    return "redirect:/result";
   }
 
   @GetMapping("/delete/{noteIdString}")
@@ -54,5 +42,17 @@ public class NoteController {
     redirectAttributes.addFlashAttribute(
         "result", ResultFactory.createSuccessResult("Note deleted successfully."));
     return "redirect:/result";
+  }
+
+  protected Result getResultForCreateOrUpdateNote(Note note) {
+    try {
+      this.noteService.createOrUpdateNote(note);
+      return ResultFactory.createSuccessResult("Note successfully created or updated.");
+    } catch (DataTooLargeException e) {
+      return ResultFactory.createErrorResult("Note contents too large. Please make note smaller.");
+    } catch (AlreadyExistsException e) {
+      return ResultFactory.createErrorResult(
+          "Note already exists. Please edit or delete the note instead.");
+    }
   }
 }
